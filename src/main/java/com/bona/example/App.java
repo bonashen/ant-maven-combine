58ffca24f0515c45b210da6fa7c9653e35215a2f
bonashen <bona.shen@gmail.com>
2015-11-23 09:05:43 +0800

    Initial commit

---
+++ src/main/java/com/bona/example/App.java
@@ -0,0 +1,13 @@
+package com.bona.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
